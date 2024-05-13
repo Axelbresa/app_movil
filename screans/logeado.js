08db52d9aa8d81c46b03ce09deb013f15c85d42b
AxelBresa <bresanovichaxel43@gmail.com>
2024-05-13 11:55:45 -0300

    Se agrego stilos a la bienvenida

--- screans/logeado.js
+++ screans/logeado.js
@@ -1,16 +1,44 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-function logeado({ route }) {
+function Logeado({ route }) {
     const { username } = route.params;
-    console.log(username)
-  return (
-    <View>
-        <Text>
-        🎆Bienvenido {username} 📱 
-        </Text>
-    </View>
-  )
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>
+                📱Bienvenido {username} 🎆
+            </Text>
+            <Text style={styles.text}>
+            🪅🎊Ahora ya puedes acceder a las funciones especiales de la aplicacion
+                Esperamos que disfrute su instacia🎊🎉
+            </Text>
+        </View>
+    );
 }
 
-export default logeado
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "green"
+    },
+    titulo: {
+        fontSize: 32,
+        color: 'blue',
+        borderWidth: 2,
+        padding: 10,
+        margin:10,
+        backgroundColor: "white"
+    },
+    text: {
+        fontSize: 19,
+        color: 'black',
+        borderWidth: 1,
+        padding: 15,
+        backgroundColor: "white"
+    },
+});
+
+export default Logeado;
